08809e9eccedb249cb9ca716537ff9befe72dac5
Bloomy <90511192+BloomyInDev@users.noreply.github.com>
2023-03-28 17:43:13 +0200

    Remove yseless files and adding an mention to me
    I'm not narcissistic of me, it's just that i want people to see that it's me that make this

--- game.js
+++ game.js
@@ -1,14 +1,11 @@
 function chooseWord() {
-    //console.log(listword);
     let randomWord = listword[Math.floor(Math.random() * listword.length)];
     var rerool = true;
     while (rerool) {
         if (randomWord.length>4 && randomWord.length<10) {
             rerool = false;
-            console.log("Word chosen, it's '"+randomWord+"'")
         } else {
             randomWord = listword[Math.floor(Math.random() * listword.length)];
-            console.log("Reroll")
         }
     }
     return randomWord
@@ -19,13 +16,12 @@ function addToProposition(){
         if (userWord.length === theWord.length) {
             tryno++
             columntable = ["no1","no2","no3","no4","no5"]
-            console.log("In addToProposition()")
             var line = document.getElementById(columntable[tryno])
             finalOut = ""
             if (userWord == theWord) {
                 finalOut = `<a id="good">${userWord}</a>`
                 alert(`Partie gagnée, le mot était ${theWord} trouvé en ${tryno} essai(s)`)
-                window.location.href = "/";
+                window.location.href = "../";
             } else {
                 for (var i = 0; i < userWord.length; i++) {
                     if (theWord[i] == userWord[i]) {
@@ -36,7 +32,6 @@ function addToProposition(){
                         finalOut+=`<a id="bad">${userWord[i]}</a>`
                     }
                 }
-                //console.log(finalOut)
             }
             line.innerHTML = finalOut
         } else if (userWord.length != 0) {
@@ -44,14 +39,13 @@ function addToProposition(){
         }
     } else {
         alert(`Partie perdue, le mot était ${theWord}`)
-        window.location.href = "/";
+        window.location.href = "../";
     }
 }
 
 var tryno = -1
 var theWord = chooseWord().toUpperCase()
 document.getElementById("lengthword").innerHTML = `Le mot est de longueur ${theWord.length}`
-//document.getElementById("thing").innerHTML = theWord;
 document.getElementsByClassName("load")[0].classList.add("hide")
 document.getElementsByClassName("game")[0].classList.remove("hide")
 const textzone = document.getElementById("typezone");
@@ -62,6 +56,4 @@ textzone.addEventListener("keyup", function(event) {
         addToProposition()
     }
 });
-//var button = document.getElementById("button");
-//button.addEventListener("click",addToProposition())
 
