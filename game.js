8ce9d5b7ce10ca8036738dfa4de05cfb5202838f
Bloomy <90511192+BloomyInDev@users.noreply.github.com>
2023-05-27 22:31:14 +0200

    Make everything better
    - Compatibility with phones is now done
    - Modified how the words are shown
    - Build with Vite.js now

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ function chooseWord() {
     let randomWord = listword[Math.floor(Math.random() * listword.length)];
     var rerool = true;
     while (rerool) {
-        if (randomWord.length>4 && randomWord.length<10 && randomWord === randomWord.toLowerCase()) {
+        if (randomWord.length>4 && randomWord.length<8 && randomWord === randomWord.toLowerCase()) {
             rerool = false;
         } else {
             randomWord = listword[Math.floor(Math.random() * listword.length)];
@@ -17,47 +17,57 @@ function copyList(originalList){
     }
     return newList
 }
-function addToProposition(){
-    var userWord = document.getElementById("typezone").value.toUpperCase();
-    if (tryno != 4) {
-        if (userWord.length === theWord.length) {
-            tryno++
-            columntable = ["no1","no2","no3","no4","no5"]
-            var line = document.getElementById(columntable[tryno])
-            finalOut = ""
-            if (userWord == theWord) {
-                finalOut = `<a id="good">${userWord}</a>`
-                alert(`Partie gagnée, le mot était ${theWord} trouvé en ${tryno} essai(s)`)
-                history.back()
-            } else {
-                testFromUser = copyList(listOfTheWord)
-                for (var i = 0; i < userWord.length; i++) {
-                    
-                    //console.log(userWord[i], testFromUser.indexOf(userWord[i]))
-                    if (testFromUser.indexOf(userWord[i]) == i) {
-                        if (lettersDiscovered[i] == -1) {lettersDiscovered.splice(i,1,theWord[i])}
-                        testFromUser[i]=-1
-                        finalOut+=`<a id="good">${userWord[i]}</a>`
-                    } else if (testFromUser.indexOf(userWord[i]) != -1) {
-                        testFromUser[i]=-1
-                        finalOut+=`<a id="inword">${userWord[i]}</a>`
-                    } else {
-                        finalOut+=`<a id="bad">${userWord[i]}</a>`
-                    }
-                    //console.log(testFromUser)
-                }
-            }
-            line.innerHTML = finalOut
-        } else if (userWord.length != 0) {
-            alert(`Le mot proposé n'est pas de la bonne longueur (longeure requise => ${theWord.length})`)
+function updatehintzone(){
+    console.log(lettersDiscovered)
+    var line = document.getElementsByClassName('hint')[0]
+    line.innerHTML = ''
+    for (var i = 0; i < lettersDiscovered.length; i++) {
+        let newDiv = document.createElement('div')
+        newDiv.classList.add('column')
+        if (lettersDiscovered[i] != -1){
+            newDiv.innerText = lettersDiscovered[i]
+            newDiv.classList.add('good')
+        } else {
+            newDiv.innerText = '*'
+            newDiv.classList.add('gray')
         }
-    } else {
-        alert(`Partie perdue, le mot était ${theWord}`)
-        history.back()
+        line.appendChild(newDiv)
     }
 }
-
-var tryno = -1
+function addToProposition(){
+    var userWord = document.getElementById("typezone").value.toUpperCase();
+    console.log(userWord, tryno)
+    if (userWord.length === theWord.length){
+        console.log("coucou")
+        let line = document.getElementsByClassName('propositions')
+        console.log(line.length)
+        if (tryno <= line.length) {
+            let actualLine = line[tryno]
+            actualLine.classList.add('border')
+            testFromUser = copyList(listOfTheWord)
+            for (var i = 0; i < userWord.length; i++) {
+                let newDiv = document.createElement('div')
+                newDiv.innerText = userWord[i]
+                newDiv.classList.add('column')
+                //console.log(userWord[i], testFromUser.indexOf(userWord[i]))
+                if (testFromUser.indexOf(userWord[i]) == i) {
+                    if (lettersDiscovered[i] == -1) {lettersDiscovered.splice(i,1,theWord[i])}
+                    testFromUser[i]=-1
+                    newDiv.classList.add('good')
+                } else if (testFromUser.indexOf(userWord[i]) != -1) {
+                    testFromUser[i]=-1
+                    newDiv.classList.add('inword')
+                } else {
+                    newDiv.classList.add('bad')
+                }
+                actualLine.appendChild(newDiv)
+            }
+        }
+        tryno++
+    }
+    updatehintzone()
+}
+var tryno = 0
 var theWord = chooseWord().toUpperCase()
 var listOfTheWord = copyList(theWord)
 var lettersDiscovered = []
@@ -67,15 +77,17 @@ for (var i = 0; i < theWord.length; i++) {
 }
 //console.log(listOfTheWord)
 document.getElementById("lengthword").innerHTML = `Le mot est de longueur ${theWord.length}`
-document.getElementById("hint").innerHTML = '<a id="good">'+theWord[0]+'</a><a id="gray">'+'*'.repeat(theWord.length-1)+"</a>"
+//document.getElementById("hint").innerHTML = '<a id="good">'+theWord[0]+'</a><a id="gray">'+'*'.repeat(theWord.length-1)+"</a>"
+
 document.getElementsByClassName("load")[0].classList.add("hide")
 document.getElementsByClassName("game")[0].classList.remove("hide")
 const textzone = document.getElementById("typezone");
 textzone.value = ""
 textzone.maxLength = theWord.length
+updatehintzone()
 textzone.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
-        addToProposition()
+        addToProposition2()
     }
 });
 document.getElementById("hintbutton").addEventListener('click', function(event) {
@@ -90,11 +102,7 @@ document.getElementById("hintbutton").addEventListener('click', function(event) 
                 }
             }
             lettersDiscovered.splice(randomLetter,1,theWord[randomLetter])
-            var finaltext = ""
-            lettersDiscovered.forEach(element => {
-                if (element != -1) {finaltext+='<a id="good">'+element+'</a>'} else {finaltext+='<a id="gray">*</a>'}
-            });
-            document.getElementById("hint").innerHTML = finaltext
+            updatehintzone()
             alert("Votre indice est arrivé !")
         }
     }
