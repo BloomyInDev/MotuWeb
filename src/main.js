19bb0d476d3f84176af21e66b5b48b030f342a71
Bloomy <90511192+BloomyInDev@users.noreply.github.com>
2023-06-06 19:39:28 +0200

    Making everything better
    - Transition to Vue.js 3, Vite (fully) and Sass
    - Adding Bootstrap cuz i'm to lazy to make a good theme
    - Adding statistics of win, lose, winstreak and ratio win/lose
    - Adding better integration of the typezone

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/style.css'
+import './scss/style.scss'
+// eslint-disable-next-line no-unused-vars
+import * as bootstrap from 'bootstrap'
+
 createApp(App).mount('#app')
