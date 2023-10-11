fd75b4f855df1eab29cf3b641281e99807d9da14
Bloomy <90511192+BloomyInDev@users.noreply.github.com>
2023-10-11 11:57:40 +0200

    Change to deploy to my subdomain

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/MotuWeb/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
