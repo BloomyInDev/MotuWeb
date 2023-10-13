2f292e2f22ccbb039683053833b4bb746a1e4232
Bloomy <90511192+BloomyInDev@users.noreply.github.com>
2023-10-13 08:57:24 +0200

    Init Preact
    - Change to Preact (testing)

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './scss/style.scss'
-// eslint-disable-next-line no-unused-vars
-import * as bootstrap from 'bootstrap'
-
-createApp(App).mount('#app')
